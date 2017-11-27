db050deeaaff709b11a2939b19c3ba452ad94b49
elzup <guild0105@gmail.com>
2017-11-27 17:31:52 +0900

    Update about section

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -10,16 +10,34 @@ const Layout = ({ name = 'Section' }: Props) => (
     <table>
       <tbody>
         <tr>
-          <td>c</td>
-          <td>a</td>
+          <th>社名</th>
+          <td>株式会社PlanckUnits(プランクユニッツ)</td>
         </tr>
         <tr>
-          <td>b</td>
-          <td>b</td>
+          <th>オフィス</th>
+          <td>〒120-0034 東京都足立区千住3-6ツォード千住壱番館</td>
         </tr>
         <tr>
-          <td>b</td>
-          <td>b</td>
+          <th>事業内容</th>
+          <td>
+            <ul>
+              <li>IoT開発</li>
+              <li>Webシステム・アプリケーション開発</li>
+              <li>コンサルティング・教育</li>
+            </ul>
+          </td>
+        </tr>
+        <tr>
+          <th>代表取締役</th>
+          <td>野中 直樹</td>
+        </tr>
+        <tr>
+          <th>資本金</th>
+          <td>7,500,000</td>
+        </tr>
+        <tr>
+          <th>従業員数</th>
+          <td>3人</td>
         </tr>
       </tbody>
     </table>
