d28cbb1e6e94970168426045a87385813b99b0de
elzup <guild0105@gmail.com>
2017-11-27 20:03:08 +0900

    Navbar style

--- components/Header.js
+++ components/Header.js
@@ -7,23 +7,32 @@ const Header = () => (
   <header>
     <nav>
       <h1>
-        <a href="/" className="brand">
+        <div className="brand">
           <Logo />
           <span>PlanckUnits</span>
-        </a>
+        </div>
       </h1>
     </nav>
     <style jsx>{`
+      h1 {
+        margin: 0;
+      }
       .brand {
         display: flex;
       }
+      span {
+        line-height: 44px;
+      }
       nav {
+        padding: 10px;
+        display: flex;
       }
     `}</style>
     <style jsx global>{`
       body {
         padding: 0;
         margin: 0;
+        font-family: 'Exo 2', 'Helvetica Neue', Helvetica, Arial, sans-serif;
       }
     `}</style>
   </header>
