940c91ab92af409d2e725966646f23d534f33e3f
elzup <guild0105@gmail.com>
2017-11-27 19:51:51 +0900

    EyeCatch style

--- components/EyeCatch.js
+++ components/EyeCatch.js
@@ -1,5 +1,8 @@
 const EyeCatch = () => (
   <section>
+    <div className="image-wrap">
+      <img src="static/logo-name.svg" alt="株式会社プランクユニッツ" />
+    </div>
     <div>
       <p>IoTとクラウド技術でのオーダーメイド開発</p>
     </div>
@@ -8,7 +11,18 @@ const EyeCatch = () => (
         padding: 100px 0;
         font-size: 2em;
         text-align: center;
-        background: gray;
+        background: #223740;
+        color: white;
+      }
+      .image-wrap {
+        align-items: center;
+        width: 100%;
+        display: flex;
+      }
+      img {
+        width: 600px;
+        height: 600px;
+        margin: 0 auto;
       }
     `}</style>
   </section>
