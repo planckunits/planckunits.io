285ed6dac2a1b8990febed48af492a4879448060
elzup <guild0105@gmail.com>
2017-11-28 16:28:29 +0900

    Add meta infos

--- components/Layout.js
+++ components/Layout.js
@@ -9,9 +9,27 @@ type Props = { children?: any, title?: string }
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
   <div>
     <Head>
-      <title />
+      <title>PlanckUnits</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="description"
+        content="株式会社プランクユニッツの会社概要を掲載しています。"
+      />
+
+      <meta property="og:title" content="株式会社プランクユニッツ" />
+      <meta
+        property="og:description"
+        content="株式会社プランクユニッツの会社概要を掲載しています。"
+      />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="format-detection" content="telephone=no" />
+
+      <link
+        rel="shortcut icon"
+        type="image/x-icon"
+        href="/static/favicon.ico"
+      />
     </Head>
     <Header />
     <style jsx>{`
