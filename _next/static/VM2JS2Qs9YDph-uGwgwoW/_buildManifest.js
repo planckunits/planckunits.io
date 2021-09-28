d60ed65d1636eb28cfc108a5a1d1511dc51fe916
elzup <guild0105@gmail.com>
2021-09-28 10:49:30 +0900

    Updates

---
+++ _next/static/VM2JS2Qs9YDph-uGwgwoW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/716-a6de76b4cb64dccc44a2.js","static/chunks/pages/index-f7a996c2e56705cf7aa7.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
